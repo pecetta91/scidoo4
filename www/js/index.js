db0447ce03d167f80a8d17e848abdb0463b31e1d
pecetta91 <info@nonnamaria.net>
2017-07-16 12:26:58 +0200

    c5

--- www/js/index.js
+++ www/js/index.js
@@ -58,22 +58,3 @@ var app = {
 
 app.initialize();
 
-
-document.addEventListener('deviceready', function () {
-  // Enable to debug issues.
-  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-  
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
-
-  window.plugins.OneSignal
-    .startInit("5870b141-9a5c-4a3e-ad4d-ba95836b1ffa")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
-  
-  // Call syncHashedEmail anywhere in your app if you have the user's email.
-  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
-  // window.plugins.OneSignal.syncHashedEmail(userEmail);
-}, false);
-
