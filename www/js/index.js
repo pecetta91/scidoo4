8d00dd8458e371cd361d15a2df98b953f5b1587f
pecetta91 <info@nonnamaria.net>
2017-07-16 12:02:18 +0200

    c2

--- www/js/index.js
+++ www/js/index.js
@@ -26,7 +26,7 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
@@ -34,14 +34,15 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        var notificationOpenedCallback = function(jsonData) {
+        
+		/*var notificationOpenedCallback = function(jsonData) {
           alert('Notifica');
         };
 
         window.plugins.OneSignal
           .startInit("5870b141-9a5c-4a3e-ad4d-ba95836b1ffa")
           .handleNotificationOpened(notificationOpenedCallback)
-          .endInit();
+          .endInit();*/
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -55,3 +56,23 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("YOUR_APPID")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+  
+  // Call syncHashedEmail anywhere in your app if you have the user's email.
+  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+  // window.plugins.OneSignal.syncHashedEmail(userEmail);
+}, false);
