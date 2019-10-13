0d20806fdb480246b51dadc74938c1617b9ea880
Alessandro Ciriaci <info>
2019-10-13 12:40:08 +0200

    ss

--- www/js/index.js
+++ www/js/index.js
@@ -31,7 +31,7 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);*/
-		myApp.showIndicator();
+		//myApp.showIndicator();
 		
 				
 		
